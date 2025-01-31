50cf0bdb6f8607ed64de168bd4e22a9417e9dc41
Aisha <145033022+Aisha-syed9@users.noreply.github.com>
2025-02-01 00:26:19 +0400

    Update home.py

--- home.py
+++ home.py
@@ -8,9 +8,9 @@ root.geometry("800x600")  # Adjust size as needed
 
 # Function to go to the account page
 def go_to_account_page():
-    # Create the account page and switch to it
+    # Creates the account page and switch to it
     account_page = AccountPage(root)
-    account_page.pack(fill="both", expand=True)  # Make sure this is packed
+    account_page.pack(fill="both", expand=True)  # Makes sure this is packed
     # Hide the home page content when switching to account page
     home_page.pack_forget()
 
@@ -21,7 +21,7 @@ home_page.pack(fill="both", expand=True)
 # Set background image for the home page
 background_image = tk.PhotoImage(file="images\Start page.png")  #image file path
 background_label = tk.Label(home_page, image=background_image)
-background_label.place(relwidth=1, relheight=1)  # Cover the entire window
+background_label.place(relwidth=1, relheight=1)  # Covers the entire window
 
 # Keep reference to the image to prevent it from being garbage collected
 home_page.image_ref = background_image
